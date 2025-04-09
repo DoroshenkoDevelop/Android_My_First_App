734c8ca656a632fefd0f718e5963008040668b57
doroshenkodevelop <printcodestudio@gmail.com>
2025-04-09 13:02:56 +0300

    Android Lesson and Homework 2

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.myapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -10,5 +13,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.button).setOnClickListener {
+            Toast.makeText(this, "Кнопка нажата", Toast.LENGTH_SHORT).show()
+        }
     }
 }
