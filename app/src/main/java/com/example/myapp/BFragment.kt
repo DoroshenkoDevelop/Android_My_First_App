7a3567107e9c42bdd18086bdb2d59adb86f229f8
doroshenkodevelop <printcodestudio@gmail.com>
2025-04-17 21:36:45 +0300

    Android Lesson 3 Myfragment replace

---
+++ app/src/main/java/com/example/myapp/BFragment.kt
@@ -0,0 +1,37 @@
+package com.example.myapp
+
+import android.graphics.Color
+import android.os.Bundle
+import android.view.Gravity
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [BFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class BFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return TextView(requireContext()).apply {
+            text = "B"
+            textSize = 24f
+            gravity = Gravity.CENTER
+            setTextColor(Color.RED)
+        }
+
+    }
+}
--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.myapp
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -10,5 +11,29 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        /*Задача 3: Замена одного фрагмента на другой (replace)
+        Задание: В MainActivity есть два фрагмента (FragmentA, FragmentB). При нажатии
+        на кнопку заменяй FragmentA на FragmentB*/
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.fragment_container, AFragment())
+                .commit()
+        }
+        findViewById<Button>(R.id.btn_replace).setOnClickListener {
+            // Заменяем FragmentA на FragmentB
+            supportFragmentManager.beginTransaction()
+                .setCustomAnimations(
+                    android.R.anim.fade_in,
+                    android.R.anim.fade_out
+                )
+                .replace(R.id.fragment_container, BFragment())
+                .addToBackStack("fragment_b")
+                .commit()
+        }
+        /*Задача 3: Замена одного фрагмента на другой (replace)
+                Задание: В MainActivity есть два фрагмента (FragmentA, FragmentB). При нажатии
+                на кнопку заменяй FragmentA на FragmentB*/
     }
 }
