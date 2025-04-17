96866e5942e8fc63d8529ca60d031765cc491a67
doroshenkodevelop <printcodestudio@gmail.com>
2025-04-17 20:57:51 +0300

    Android Lesson 3 Myfragment

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -10,5 +10,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+       /* Задача 2:  Добавление фрагмента в контейнер (FragmentManager)
+        Задание: Добавь MyFragment в контейнер FrameLayout*/
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.fragment_container, MyFragment())
+                .commit()
+        }
+        /* Задача 2:  Добавление фрагмента в контейнер (FragmentManager)
+        Задание: Добавь MyFragment в контейнер FrameLayout*/
     }
 }
---
+++ app/src/main/java/com/example/myapp/MyFragment.kt
@@ -0,0 +1,35 @@
+package com.example.myapp
+
+import android.os.Bundle
+import android.view.Gravity
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [MyFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class MyFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return TextView(requireContext()).apply {
+            text = "MyFragment!"
+            textSize = 24f
+            gravity = Gravity.CENTER
+        }
+    }
+
+}
